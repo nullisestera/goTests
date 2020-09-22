7a13c5d99df3a57b0bc9e5c14c1bb5b18f803aa5
Ulises Vargas <>
2020-09-22 16:40:32 -0400

    Statements & Functions

--- main.go
+++ main.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades" // Static way
-	card := "Ace of Spades"   // Dynamic way
-	card = "Five of Diamonds" // Redeclare variables, doesn't need colon (:)
+	// card := "Ace of Spades"   // Dynamic way
+	// card = "Five of Diamonds" // Redeclare variables, doesn't need colon (:)
+
+	card := newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of spades"
 }
