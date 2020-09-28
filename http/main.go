7b55ecf8b68f7997af04839e33540ec9de304650
Ulises Vargas <>
2020-09-28 07:47:24 -0400

    HTTP GET

--- http/main.go
+++ http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	// GET ENDPOINT HTTP
 	res, err := http.Get("https://jsonplaceholder.typicode.com/posts")
@@ -15,7 +17,23 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	// Calling io.Copy 
+	// Calling io.Copy
 	// function to copy res.Body Reader interface to os.Stdout (output) using res.Body Writer interface
-	io.Copy(os.Stdout, res.Body)
+	// Copy (dst Writer, src Reader)
+	// dst: destiny
+	// src: source
+	// io.Copy(os.Stdout, res.Body)
+
+	lw := logWriter{}
+	io.Copy(lw, res.Body)
+}
+
+// Custom Writer
+func (logWriter) Write(bs []byte) (int, error) {
+	// print the byte slice returned by the Reader interface
+	fmt.Println(string(bs))
+
+	// return length of byteslice
+	fmt.Println("Length of Byte Slice", len(bs))
+	return len(bs), nil
 }
